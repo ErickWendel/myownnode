5d8e876f619d0514732fd19c989dce1e8480ca4b
Erick Wendel <erick.workspace@gmail.com>
2022-09-12 21:55:26 -0300

    add complex ops
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-print('printing.. Hey dude!')
+print('printing.. 1Hey dude!')
+print('printing.. 2Hey dude!')
+print('printing.. 3Hey dude!')
+print('printing.. 4Hey dude!')
 
-const setTimeout = (cb, ms) => timeout(ms, 10, cb);
-const interval = (cb, ms) => timeout(10, ms, cb);
+// const setTimeout = (cb, ms) => timeout(ms, 10, cb);
+// const interval = (cb, ms) => timeout(10, ms, cb);
 
-timeout(100, 0, function () {
-  print('ran after 1s')
-  timeout(100, 0, function () {
-    print('ran after 2s')
-  }, 2000);
 
-}, 1000);
+for(let i=0; i< 10; i++) {
+  print('ae', i);
+}
+
+print( new Date().toISOString(), 'ran')
+timeout(1000, 0, function () {
+  print( new Date().toISOString(), 'ran after 1s')
+
+  function test() {
+    timeout(2000, 0, function () {
+      print( new Date().toISOString(), 'ran after 2s')
+    });
+  }
+  test()
+});
