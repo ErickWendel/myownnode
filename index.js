379ab227b1e675c31fa5ee314867de1f4f7291b8
Erick Wendel <erick.workspace@gmail.com>
2022-09-14 22:48:39 +0900

    index.js
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ print('printing.. 4Hey dude!')
 // const interval = (cb, ms) => timeout(10, ms, cb);
 
 
-for(let i=0; i< 10; i++) {
-  print('ae', i);
-}
+// for(let i=0; i< 10; i++) {
+//   print('ae', i);
+// }
 
-print( new Date().toISOString(), 'ran')
-timeout(1000, 0, function () {
-  print( new Date().toISOString(), 'ran after 1s')
+print(new Date().toISOString(), 'ran');
 
-  function test() {
+timeout(100, 0, function () {
+  print('1 ' + new Date().toISOString());
+  timeout(2000, 0, function () {
+    
+    print('2 ' + new Date().toISOString());
+  
     timeout(2000, 0, function () {
-      print( new Date().toISOString(), 'ran after 2s')
+      
+      print('3 ' + new Date().toISOString());
+    
     });
-  }
-  test()
+  });
 });
