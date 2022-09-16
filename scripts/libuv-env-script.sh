d40f06278773ee9f46947adc7680a7faa808e626
Erick Wendel <erick.workspace@gmail.com>
2022-09-16 07:30:58 +0000

    add env for linux x86 gitpod

--- scripts/libuv-env-script.sh
+++ scripts/libuv-env-script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-APP_DIR=../../app3
+APP_DIR=../../app
 
 git clone https://github.com/libuv/libuv
 cd libuv && git checkout v1.x
 
-mkdir -p build
+# mkdir -p build
 
-(cd build && cmake .. -DBUILD_TESTING=ON)
-cmake -j8 --build build
+# (cd build && cmake .. -DBUILD_TESTING=ON)
+# cmake -j8 --build build
 
+sh autogen.sh
+./configure
+make
+make check
+make install
 
 ./build/uv_run_tests
 
 cp -r include $APP_DIR/libuv/
-cp -r build/libuv_a.a $APP_DIR/libuv/
+cp -r build/libuv_a.a $APP_DIR/libuv/libuv_a-x86_64.a
