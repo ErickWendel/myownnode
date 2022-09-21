588afa293adcc8e64419e4b62a4016672f5bbad9
Erick Wendel <erick.workspace@gmail.com>
2022-09-21 19:30:17 +0000

    add env

--- start-x86.sh
+++ start-x86.sh
@@ -1,1 +1,13 @@
+
+if ! command -v "nodemon" /dev/null
+then
+  npm i -g nodemon --silent
+fi
+
+if ! command -v "ccache" /dev/null
+then
+  sudo apt-get install -y ccache
+fi
+
+
 nodemon -e cc,h --exec "make -f makefile-x86 && ./myownnode index.js"
